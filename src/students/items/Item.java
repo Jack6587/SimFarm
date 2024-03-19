0ac572a9d57acc2468dcb1282ca448616d56d8b3
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-03-19 22:20:46 +1030

    Modifications
    Changed age to increment with ++ rather than using += 1. Fixed a painful issue where the died() function did not work - realised there was an extra curly bracket which (I think) put it inside of another method or just really confused everything.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -3,8 +3,15 @@ package students.items;
 public class Item {
 	public int age, maturationAge, deathAge, monetaryValue;
 	
+	public Item(int maturationAge, int deathAge, int monetaryValue) {
+		this.age = 0;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monetaryValue = monetaryValue;
+	}
+	
 	public void tick() {
-		age += 1;
+		age ++;
 	}
 	
 	public void setAge(int ageValue) {
@@ -12,8 +19,6 @@ public class Item {
 		
 	public boolean died() {
 		return age > deathAge;
-	}
-	
 	}
 	
 }
