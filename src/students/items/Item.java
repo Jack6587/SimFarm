9b3abb2de536df02967c33b12e58a49b7cb20893
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-03-19 22:52:08 +1030

    Comments and method return changes
    Added comments to help with code understanding. I changed died() from returning a boolean to a String so that I could better represent death/no death, rather than just true or false

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -3,6 +3,7 @@ package students.items;
 public class Item {
 	private int age, maturationAge, deathAge, monetaryValue;
 	
+	// Constructor - age is set separately but initially = 0
 	public Item(int maturationAge, int deathAge, int monetaryValue) {
 		this.age = 0;
 		this.maturationAge = maturationAge;
@@ -10,18 +11,26 @@ public class Item {
 		this.monetaryValue = monetaryValue;
 	}
 	
+	// Increments the age by 1
 	public void tick() {
 		age ++;
 	}
 	
+	// Sets the age of the instance of Item to the parameter ageValue
 	public void setAge(int ageValue) {
 		this.age = ageValue;
 	}
-		
-	public boolean died() {
-		return age > deathAge;
+	
+	// Returns string representation for whether an item has died or not
+	public String died() {
+		if(age > deathAge) {
+			return("This item has died...");
+		} else {
+			return("This item is alive!");
+		}
 	}
 	
+	// Returns an integer, either value or 0, based on whether Item has reached maturation age or not. Must be an instance of Food class
 	public int getValue() {
 		if(age > maturationAge && this instanceof Food) {
 			return(monetaryValue);
@@ -29,5 +38,7 @@ public class Item {
 			return 0;
 		}
 	}
+	
+	
 	
 }
