07bbc3092f72e4d9ffad7d8c70b209de912b9439
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-03-19 22:10:01 +1030

    Modified Item.java
    Initiated first method development for Item class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,19 @@
 package students.items;
 
 public class Item {
-
+	public int age, maturationAge, deathAge, monetaryValue;
+	
+	public void tick() {
+		age += 1;
+	}
+	
+	public void setAge(int ageValue) {
+		this.age = ageValue;
+		
+	public boolean died() {
+		return age > deathAge;
+	}
+	
+	}
 	
 }
