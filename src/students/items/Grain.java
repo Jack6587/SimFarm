fe565f0ccbd5149da7303dd82fcf8cd35889a298
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-03-24 22:24:37 +1030

    Modified grain class
    Almost finished grain class, just brainstorming how to access age without using a getter. Likely chance is I can use a getter, but not sure yet

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,6 +1,26 @@
 package students.items;
 
-public class Grain{
-
+public class Grain extends Food {
+	private static final int maturationAge = 2;
+	private static final int deathAge = 6;
+	private static final int monetaryValue = 2;
+	private static int generationCount = 0;
+	
+	public Grain() {
+		super(maturationAge, deathAge, monetaryValue);
+		generationCount++;
+	}
+	
+	public String toString() {
+		if(Grain.age < maturationAge) {
+			return "g";
+		} else {
+			return "G";
+		}
+	}
+	
+	public static int getGenerationCount() {
+		return generationCount;
+	}
 	
 }
