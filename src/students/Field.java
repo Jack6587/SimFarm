aa7fc22bc81087a5f821c2e8e416214e2ab0a62f
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-03-30 22:23:51 +1030

    Creation code for the Field class

--- src/students/Field.java
+++ src/students/Field.java
@@ -1,13 +1,23 @@
 package students;
 
 import students.items.Item;
+import students.items.Soil;
 
 public class Field {
 	
+	// Instance variable - 2D array "field" of type Item
 	private Item[][] field;
 	
 	public Field(int height, int width) {
-		
+		// Creates a new field instance
+		field = new Item[height][width];
+	
+		// Initialised a nested for loop to iterate over height and width of the field to add an instance of Soil to each
+		for(int i = 0; i < field.length; i++) {
+			for(int j = 0; j < field[j].length; i++) {
+				field[i][j] = new Soil();
+			}
+		}
 	}
 	
 }
