2debacf1acff7e355fcdff80b46d1daacd71942f
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-03-31 14:20:23 +1030

    Established toString() method in Field class
    Started the field functionality. Have not finished tick() yet but tried to construct the output for the field, which I feel will help me visualise and better understand the approach of the other methods

--- src/students/Field.java
+++ src/students/Field.java
@@ -6,26 +6,47 @@ import students.items.Soil;
 public class Field {
 	
 	// Instance variable - 2D array "field" of type Item
+	private int height;
+	private int width;
 	private Item[][] field;
 	
 	public Field(int height, int width) {
 		// Creates a new field instance
+		this.height = height;
+		this.width = width;
 		field = new Item[height][width];
 	
 		// Initialised a nested for loop to iterate over height and width of the field to add an instance of Soil to each
 		for(int i = 0; i < height; i++) {
-			for(int j = 0; j < height; i++) {
+			for(int j = 0; j < height; j++) {
 				field[i][j] = new Soil();
 			}
 		}
 	}
 	
 	public void tick() {
-		for(int i = 0; i < field.length; i++) {
-			for(int j = 0; j < field[i].length; j++) {
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
 				field[i][j].tick();
 			}
 		}
+	}
+	
+	public String toString() {
+		StringBuilder fieldOutput = new StringBuilder();
+		
+		fieldOutput.append(" ");
+		for(int j = 0; j < width; j++) {
+			fieldOutput.append(j + " ");
+		}
+		
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				fieldOutput.append(field[i][j] + " ");
+			}
+		}
+		
+		return fieldOutput.toString();
 	}
 	
 }
