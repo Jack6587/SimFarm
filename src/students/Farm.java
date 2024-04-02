1b4c381c5996506f6e716c4aae7748c590208a21
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-04-02 13:18:17 +1030

    Initialised Farm class
    Created the constructor, initialised variables, and started to implement the run() method. It prints output as outlined in the specs, and uses input to determine the next execution. Currently I have done the simple input (single characters) which so far is straightforward.

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -1,13 +1,46 @@
 package students;
 
+import java.util.Scanner;
+
 public class Farm {
 	
-	public Farm(int fieldWidth, int fieldHeight, int startingFunds)
-	{
+	private final int startingFunds;
+	private Field field;
+
+	public Farm(int fieldWidth, int fieldHeight, int startingFunds) {
+		this.startingFunds = startingFunds;
+		this.field = new Field(fieldWidth, fieldHeight);
 	}
 	
-	public void run()
-	{
+	public void run() {
+		Scanner s = new Scanner(System.in);
+		boolean run = true;
+		
+		while(run) {
+			System.out.println(field.toString());
+			System.out.println("Bank balance: $" + startingFunds);
+			System.out.println("Enter your next action:");
+			System.out.println("t x y: till");
+			System.out.println("h x y: harvest");
+			System.out.println("p x y: plant");
+			System.out.println("s: field summary");
+			System.out.println("w: wait");
+			System.out.println("q: quit");
+			
+			String input = s.nextLine();
+			
+			if(input.equals("q")){
+				run = false;
+			}
+			if(input.equals("w")) {
+				field.tick();
+			}
+			if(input.equals("s")) {
+				field.getSummary();
+			}
+		}
+		
+		s.close();
 	}
 	
 }
