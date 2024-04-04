dbd124bcbb44029d07844c26bae85250a3189a8d
Jack6587 <klejt002@mymail.unisa.edu.au>
2024-04-04 21:06:49 +1030

    Implemented exception handling
    try and catch block that seems to mitigate any errors from occurring. Have not exhausted all possible error causes though

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -18,79 +18,94 @@ public class Farm {
 		boolean run = true;
 		
 		while(run) {
-			System.out.println(field.toString());
-			System.out.println("Bank balance: $" + startingFunds + "\n");
-			System.out.println("Enter your next action:");
-			System.out.println("t x y: till");
-			System.out.println("h x y: harvest");
-			System.out.println("p x y: plant");
-			System.out.println("s: field summary");
-			System.out.println("w: wait");
-			System.out.println("q: quit");
+			try {
+				System.out.println(field.toString());
+				System.out.println("Bank balance: $" + startingFunds + "\n");
+				System.out.println("Enter your next action:");
+				System.out.println("t x y: till");
+				System.out.println("h x y: harvest");
+				System.out.println("p x y: plant");
+				System.out.println("s: field summary");
+				System.out.println("w: wait");
+				System.out.println("q: quit");
 			
-			String input = s.nextLine();
-			String[] characters = input.split(" ");
+				String input = s.nextLine();
+				String[] characters = input.split(" ");
 			
-			if(input.equals("q")){
-				run = false;
-			}
-			
-			if(input.equals("w")) {
-				field.tick();
-			}
-			
-			if(input.equals("s")) {
-				System.out.println(field.getSummary());
-			}
-			
-			if(characters[0].equals("t")) {
-				int x = Integer.parseInt(characters[1]) - 1;
-				int y = Integer.parseInt(characters[2]) - 1;
+				if(input.equals("q")){
+					run = false;
+				}
 				
-				field.till(x, y);
-				field.tick();
+				if(input.equals("w")) {
+					field.tick();
+				}
 				
-			}
-			
-			if(characters[0].equals("h")) {
-				int x = Integer.parseInt(characters[1]) - 1;
-				int y = Integer.parseInt(characters[2]) - 1;
+				if(input.equals("s")) {
+					System.out.println(field.getSummary());
+				}
 				
-				Item harvestItem = field.get(x, y);
-				startingFunds += harvestItem.getValue();
-				field.plant(x, y, new Soil());
-				field.tick();
-			}
-			
-			if(characters[0].equals("p")) {
-				int x = Integer.parseInt(characters[1]) - 1;
-				int y = Integer.parseInt(characters[2]) - 1;
-				
-				System.out.println("Enter: \n" + "'a' to buy an apple for $3 \n" + "'g' to buy grain for $2\n");
-				
-				String plant = s.nextLine();
-				
-				if(plant.equals("a")) {
-					if(startingFunds >= 3) {
-						startingFunds -= 3;
-						Item plantApple = new Apples();
-						field.plant(x, y, plantApple);
-					} else {
-						System.out.println("Not enough funds to buy an apple.");
-					}
+				if(characters[0].equals("t")) {
+					int x = Integer.parseInt(characters[1]) - 1;
+					int y = Integer.parseInt(characters[2]) - 1;
 					
-				} else if(plant.equals("g")) {
-					if(startingFunds >= 2) {
-						startingFunds -= 2;
-						Item plantGrain = new Grain();
-						field.plant(x, y, plantGrain);
+					field.till(x, y);
+					field.tick();
+					
+				}
+				
+				if(characters[0].equals("h")) {
+					int x = Integer.parseInt(characters[1]) - 1;
+					int y = Integer.parseInt(characters[2]) - 1;
+					
+					Item harvestItem = field.get(x, y);
+					startingFunds += harvestItem.getValue();
+					field.plant(x, y, new Soil());
+					field.tick();
+				}
+				
+				if(characters[0].equals("p")) {
+					int x = Integer.parseInt(characters[1]) - 1;
+					int y = Integer.parseInt(characters[2]) - 1;
+					
+					System.out.println("Enter: \n" + "'a' to buy an apple for $3 \n" + "'g' to buy grain for $2\n");
+					
+					String plant = s.nextLine();
+					
+					if(plant.equals("a")) {
+						if(startingFunds >= 3) {
+							startingFunds -= 3;
+							Item plantApple = new Apples();
+							field.plant(x, y, plantApple);
+						} else {
+							System.out.println("Not enough funds to buy an apple.");
+						}
+						
+					} else if(plant.equals("g")) {
+						if(startingFunds >= 2) {
+							startingFunds -= 2;
+							Item plantGrain = new Grain();
+							field.plant(x, y, plantGrain);
+						} else {
+							System.out.println("Not enough funds to buy grain.");
+						}
 					} else {
-						System.out.println("Not enough funds to buy grain.");
+						System.out.println("Invalid input item type.");
 					}
 				} else {
-					System.out.println("Invalid input item type.");
+					throw new IllegalArgumentException("Invalid command");
 				}
 			}
+			catch(NumberFormatException e) {
+				System.out.println("Invalid coordinates. Please enter valid integers \n");
+			}
+			catch(IllegalArgumentException e) {
+				System.out.println("Invalid input: " + e.getMessage() + "\n");
+			}
+			catch(Exception e) {
+				System.out.println("Error: " + e.getMessage() + "\n");
+			}
+			
+			
 		}
 		s.close();
 
